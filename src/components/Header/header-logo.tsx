753749966ab1b42ac92dc82e5872b7e7d2fe60b7
Webprojon <khasanovtokhir77@gmail.com>
2024-07-31 22:55:49 +0200

    logo link connected

--- src/components/Header/header-logo.tsx
+++ src/components/Header/header-logo.tsx
@@ -1,9 +1,11 @@
 export default function HeaderLogo() {
 	return (
-		<img
-			alt="logo"
-			className="hidden w-[80px] md:block"
-			src="https://vakulenko.online/images/tild3830-6536-4238-a262-393965353063__modern-letter-n-a-v-.png"
-		/>
+		<a href="#portfolio">
+			<img
+				alt="logo"
+				className="hidden w-[80px] md:block"
+				src="https://vakulenko.online/images/tild3830-6536-4238-a262-393965353063__modern-letter-n-a-v-.png"
+			/>
+		</a>
 	);
 }
